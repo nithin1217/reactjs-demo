ea56f99059d2832f97a390a243339d75c8fab2fd
nithin1217 <108689346+nithin1217@users.noreply.github.com>
2024-01-18 15:43:05 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-docker run -p 80:80 nithin2023/reactjs-demo:latest
+docker run -p 80:80 nithin2023/reactjs:dev
 
 if [ $? -eq 0 ]; then
   echo "Docker container started successfully on the server"
