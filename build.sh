c6136e25eb5aa5c4568a087fb729e9fb4b0d99fb
nithin1217 <108689346+nithin1217@users.noreply.github.com>
2024-01-18 15:42:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker build -t nithin2023/reactjs-demo:latest .
+docker build -t nithin2023/reactjs:dev .
 
 
 if [ $? -eq 0 ]; then
-  echo "Docker image build successful: nithin2023/reactjs-demo:latest"
+  echo "Docker image build successful: nithin2023/reactjs:dev"
 else
   echo "Docker image build failed"
   exit 1
