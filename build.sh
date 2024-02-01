a1df211e73757888ac8c31c2f48bcfb1c48ad6e2
nithin1217 <108689346+nithin1217@users.noreply.github.com>
2024-02-01 13:18:59 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#docker build --no-cache -t nithin2023/reactjs:dev .
-docker build . -t reactjs-demo
+docker build --no-cache -t nithin2023/reactjs:dev .
+#docker build . -t reactjs-demo
 
-#docker push nithin2023/reactjs:dev
+docker push nithin2023/reactjs:dev
