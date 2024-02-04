c137f29cfda9b39f5608801937a383fa967a2cb0
nithin1217 <108689346+nithin1217@users.noreply.github.com>
2024-02-04 12:34:06 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker pull nithin2023/reactjs:dev
+docker pull nithin2023/reactjs:latest
 
 #docker run -d --name container2 -p 80:3000 reactjs-demo
 
-docker run -d -p 80:3000 nithin2023/reactjs:dev
+docker run -d -p 80:3000 nithin2023/reactjs:latest
