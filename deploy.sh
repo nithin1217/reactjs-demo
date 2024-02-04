41736a3f16dd787126e841e8b8a381a71fb1ea23
nithin1217 <108689346+nithin1217@users.noreply.github.com>
2024-02-04 11:52:08 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 docker pull nithin2023/reactprojectprod-1:prod
 
-docker run -d --name node-todo-app-container1 -p 80:3000 nithin2023/reactprojectprod-1:prod
+docker run -d --name containermain1 -p 80:3000 nithin2023/reactprojectprod-1:prod
