dc95c8e295ca5f5777b9293913824fa89e10e58e
nithin1217 <108689346+nithin1217@users.noreply.github.com>
2024-02-04 12:34:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build --no-cache -t nithin2023/reactprojectprod-1:prod .
+docker build --no-cache -t nithin2023/reactprojectprod-1:latest .
 #docker build . -t reactjs-demo
 
-docker push nithin2023/reactprojectprod-1:prod
+docker push nithin2023/reactprojectprod-1:latest
 
